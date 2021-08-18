e8e93f273073c00f8515676c04d870caf1133735
Manojna6 <72754918+Manojna6@users.noreply.github.com>
2021-08-18 17:10:49 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+noseX=0;
+noseY=0;
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/FRKBhnMp/image.png');
+}
+function setup()
+{
+    canvas = createCanvas(400, 400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400,400);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw() {
+    image(video, 0,0,400,500);
+    image(clown_nose, noseX-20, noseY+25, 50,50);
+}
+function take_snasphot()
+{
+    save("ImageFilterClown.png");
+}
+function modelLoaded() {
+    console.log("poseNet is Initialized");
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+    }
+}
